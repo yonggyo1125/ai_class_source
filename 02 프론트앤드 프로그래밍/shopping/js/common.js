e824547e514fc48450e37c726ff56fa858e12cc7
yonggyo1125 <yonggyo00@kakao.com>
2025-04-04 12:05:10 +0900

    .

---
+++ 02 프론트앤드 프로그래밍/shopping/js/common.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", function() {
+    new Swiper(".main-banner", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+});
