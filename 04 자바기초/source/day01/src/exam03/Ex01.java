4c503394e94df47d5e0a774d6cbe9bdf3fecc044
yonggyo1125 <yonggyo00@kakao.com>
2025-04-18 12:36:39 +0900

    .

---
+++ 04 자바기초/source/day01/src/exam03/Ex01.java
@@ -0,0 +1,10 @@
+package exam03;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num; // 변수 선언, 값을 담을 수 있는 공간(메모리) 할당
+        num = 100; // 값을 대입, 최초로 값을 저장하는 것 - 초기화
+
+        int num2 = 100; // 선언과 동시에 초기화
+    }
+}
---
+++ 04 자바기초/source/day01/src/exam03/Ex02.java
@@ -0,0 +1,12 @@
+package exam03;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        /*
+        int num1;
+        int num2;
+        int num3; */
+        int num1, num2, num3;
+        int num4 = 100, num5 = 20, num6;
+    }
+}
