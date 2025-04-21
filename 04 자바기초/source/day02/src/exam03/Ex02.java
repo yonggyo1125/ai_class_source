6ed657d86e5b52bc318a9e3b4847e458154823a3
yonggyo1125 <yonggyo00@kakao.com>
2025-04-21 17:21:37 +0900

    .

---
+++ 04 자바기초/source/day02/src/exam03/Ex02.java
@@ -0,0 +1,14 @@
+package exam03;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int result2 = add(10, 20);
+        System.out.println(result2);
+    }
+
+    static int add(int num1, int num2) {
+        int result = num1 + num2;
+
+        return result;
+    }
+}
--- 04 자바기초/source/day02/src/exam03/Student.java
+++ 04 자바기초/source/day02/src/exam03/Student.java
@@ -4,4 +4,8 @@ public class Student {
     int id; // 학번
     String name; // 이름
     String major; // 전공
+
+    void study() {
+        System.out.printf("id:%d, name:%s, major: %s%n", id, name, major);
+    }
 }
