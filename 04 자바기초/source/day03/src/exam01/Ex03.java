d31ff0be61ba1eaabf87831f05021de1e8014ba6
yonggyo1125 <yonggyo00@kakao.com>
2025-04-22 12:02:56 +0900

    .

--- 04 자바기초/source/day03/src/exam01/Ex03.java
+++ 04 자바기초/source/day03/src/exam01/Ex03.java
@@ -2,10 +2,11 @@ package exam01;
 
 public class Ex03 {
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.id = 1000;
-        s1.name = "이이름";
-        s1.major = "컴퓨터과학";
+        Student s1 = new Student(1000, "이이름", "컴퓨터과학"); // 생성자 함수 - 객체를 생성하는 역할
         s1.study();
+
+        Student s2 = new Student(1001, "김이름", "컴퓨터과학");
+        s2.study();
+        System.out.println(s1 == s2);
     }
 }
--- 04 자바기초/source/day03/src/exam01/Student.java
+++ 04 자바기초/source/day03/src/exam01/Student.java
@@ -5,6 +5,33 @@ public class Student {
     String name; // 이름
     String major; // 전공과목
 
+    /**
+     * 기본 생성자
+     * - 정의가 안되어 있으면 public Student() {}
+     */
+    /*
+    public Student() {
+        // 객체 생성 이후에 실행되는 코드..
+        /**
+         * 객체 생성 이후에는 변수 공간에 값을 최초로 넣어주는 초기화 작업을 주로 진행
+         *
+         */
+/*
+        id = 1000;
+        name = "이이름";
+        major = "컴퓨터 과학";
+    }
+    */
+    public Student() {
+
+    }
+
+    public Student(int _id, String _name, String _major) {
+        id = _id;
+        name = _name;
+        major = _major;
+    }
+
     void study() {
         System.out.printf("id=%d, name=%s, major=%s%n", id, name, major);
     }
