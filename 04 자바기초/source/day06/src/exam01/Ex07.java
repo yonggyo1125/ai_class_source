a94cd27f9a6ae2c828ba15bc5deafffbaee07be2
yonggyo1125 <yonggyo00@kakao.com>
2025-04-25 12:25:44 +0900

    .

---
+++ 04 자바기초/source/day06/src/exam01/Ex07.java
@@ -0,0 +1,8 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        LoginService service = new LoginService();
+        service.process("user02", "1234");
+    }
+}
