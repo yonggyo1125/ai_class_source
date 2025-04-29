cc93898476a00689c7bb7f011846f33138eda72d
yonggyo1125 <yonggyo00@kakao.com>
2025-04-29 16:53:34 +0900

    .

---
+++ 04 자바기초/source/day08/src/exam02/Ex07.java
@@ -0,0 +1,23 @@
+package exam02;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        ArrayList<String> items = new ArrayList<>();
+        items.add("이름1");
+        items.add("이름2");
+        items.add("이름3");
+        items.add("이름4");
+        items.add("이름5");
+
+        ListIterator<String> iter = items.listIterator();
+        System.out.println("---- 순방향 조회 ------");
+        while(iter.hasNext()) {
+            String item = iter.next();
+            int index = iter.nextIndex();
+            System.out.printf("item:%s, index:%d%n", item, index);
+        }
+    }
+}
