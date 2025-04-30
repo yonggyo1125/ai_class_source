880e1d3685d6a9f1ca5af3a35182e90e4e5eb2d8
yonggyo1125 <yonggyo00@kakao.com>
2025-04-30 10:20:22 +0900

    .

--- 04 자바기초/source/day08/src/exam03/Ex05.java
+++ 04 자바기초/source/day08/src/exam03/Ex05.java
@@ -1,11 +1,19 @@
 package exam03;
 
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Ex05 {
     public static void main(String[] args) {
-        TreeSet<String> items = new TreeSet<>();
-        items.add("이름3"); // 오름차순 정렬
+        Comparator<String> comp = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        TreeSet<String> items = new TreeSet<>(comp);
+        items.add("이름3");
         items.add("이름3");
         items.add("이름3");
         items.add("이름4");
