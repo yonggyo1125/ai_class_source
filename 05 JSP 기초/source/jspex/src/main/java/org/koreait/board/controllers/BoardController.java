9f8166b033bfc71b2cff3b593741e16ef4a71887
yonggyo1125 <yonggyo00@kakao.com>
2025-05-12 17:25:11 +0900

    .

--- 05 JSP 기초/source/jspex/src/main/java/org/koreait/board/controllers/BoardController.java
+++ 05 JSP 기초/source/jspex/src/main/java/org/koreait/board/controllers/BoardController.java
@@ -2,6 +2,7 @@ package org.koreait.board.controllers;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,7 +12,7 @@ import org.koreait.board.services.BoardInfoService;
 import java.io.IOException;
 import java.util.List;
 
-
+@WebServlet("/board/list")
 public class BoardController extends HttpServlet {  // 컨트롤러
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,8 +22,8 @@ public class BoardController extends HttpServlet {  // 컨트롤러
         req.setAttribute("items", items);
 
 
-        // 뷰(View)
-        RequestDispatcher rd = req.getRequestDispatcher("/board/list.jsp");
+        // 뷰(View) - 직접 접근을 못하도록 /WEB-INF 하위에 구성한다.
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/board/list.jsp");
         rd.forward(req, resp);
     }
 }
