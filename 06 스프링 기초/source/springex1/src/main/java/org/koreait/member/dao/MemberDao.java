3df2058d266d8d1ade07a32cc173d6ebfd785bbc
yonggyo1125 <yonggyo00@kakao.com>
2025-05-13 13:18:25 +0900

    .

--- 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/dao/MemberDao.java
+++ 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/dao/MemberDao.java
@@ -2,6 +2,7 @@ package org.koreait.member.dao;
 
 import org.koreait.member.entities.Member;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,7 +12,8 @@ public class MemberDao {
     private Map<String, Member> members = new HashMap<>();
 
     public void register(Member member) {
-        members.put(member.getEmail(), member);
+        member.setRegDt(LocalDateTime.now());
+        members.putIfAbsent(member.getEmail(), member);
     }
 
     public Member get(String email) {
--- 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/entities/Member.java
+++ 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/entities/Member.java
@@ -2,10 +2,13 @@ package org.koreait.member.entities;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Member {
     private long seq;
     private String email;
     private String password;
     private String name;
+    private LocalDateTime regDt;
 }
