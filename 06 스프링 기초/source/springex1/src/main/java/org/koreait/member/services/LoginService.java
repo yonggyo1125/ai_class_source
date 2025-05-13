8e81b5803c2704a5836f937a5010d6904c03a2fb
yonggyo1125 <yonggyo00@kakao.com>
2025-05-13 13:20:54 +0900

    .

--- 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/services/LoginService.java
+++ 06 스프링 기초/source/springex1/src/main/java/org/koreait/member/services/LoginService.java
@@ -1,7 +1,21 @@
 package org.koreait.member.services;
 
-import org.koreait.member.validators.JoinValidator;
+import org.koreait.member.RequestLogin;
+import org.koreait.member.dao.MemberDao;
+import org.koreait.member.validators.LoginValidator;
 
 public class LoginService {
+    private LoginValidator validator;
+    private MemberDao memberDao;
 
+    public LoginService(LoginValidator validator, MemberDao memberDao) {
+        this.memberDao = memberDao;
+        this.validator = validator;
+    }
+
+    public void process(RequestLogin form) {
+        validator.validate(form);
+
+        System.out.println("로그인 완료");
+    }
 }
