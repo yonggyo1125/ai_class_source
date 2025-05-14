a5748e4231f99fc554dc93d027118999242f11fd
yonggyo1125 <yonggyo00@kakao.com>
2025-05-14 12:21:44 +0900

    .

--- 06 스프링 기초/source/springex2/src/main/java/org/koreait/lifecycle/Message.java
+++ 06 스프링 기초/source/springex2/src/main/java/org/koreait/lifecycle/Message.java
@@ -1,6 +1,13 @@
 package org.koreait.lifecycle;
 
-public class Message {
+import org.springframework.beans.factory.InitializingBean;
+
+public class Message implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet() 호출!");
+    }
 
     public void send(String name) {
         System.out.printf("%s님에게 메세지 전송%n", name);
