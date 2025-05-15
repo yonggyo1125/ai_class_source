e74154379671a1bff1383b49709c8ebab44b3544
yonggyo1125 <yonggyo00@kakao.com>
2025-05-15 12:21:17 +0900

    .

---
+++ 06 스프링 기초/source/springex3/src/main/java/org/koreait/global/configs/DBConfig.java
@@ -0,0 +1,17 @@
+package org.koreait.global.configs;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfig {
+
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+
+
+        return ds;
+    }
+}
