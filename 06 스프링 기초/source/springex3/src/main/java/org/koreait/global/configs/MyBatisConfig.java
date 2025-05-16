9ed1c06a0a82a43a1fe3df5d6b828d0c9385b33c
yonggyo1125 <yonggyo00@kakao.com>
2025-05-16 11:50:21 +0900

    .

---
+++ 06 스프링 기초/source/springex3/src/main/java/org/koreait/global/configs/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package org.koreait.global.configs;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("org.koreait")
+public class MyBatisConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+
+        return factoryBean.getObject();
+    }
+}
