5800758070f7273d9d6091743eeaf9b5914c86e8
yonggyo1125 <yonggyo00@kakao.com>
2025-05-16 16:16:33 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/member/controllers/MemberController.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/member/controllers/MemberController.java
@@ -3,17 +3,33 @@ package org.koreait.global.member.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
 
-    @GetMapping("/member/register")
-    public String joinPage(Model model) {
+    @GetMapping("/member/join")
+    public String joinPage() {
 
+        return "member/join"; // 템플릿 경로
+    }
+
+    @PostMapping("/member/join")
+    public String joinPs() {
+
+        return "member/join_ps";
+    }
+
+    /*
+    @GetMapping("/member/register")
+    public String joinPage(@RequestParam("agree") boolean agree, @RequestParam("email") String email, Model model) {
+        System.out.printf("agree:%s, email:%s%n", agree, email);
         model.addAttribute("message", "안녕하세요.");
         return "member/join"; // /WEB-INF/templates/member/join.jsp -> ModelAndView 변환
     }
+    */
 
     /*
     @GetMapping("/member/register")
--- 06 스프링 기초/source/springex4/src/main/webapp/WEB-INF/templates/member/join.jsp
+++ 06 스프링 기초/source/springex4/src/main/webapp/WEB-INF/templates/member/join.jsp
@@ -1,3 +1,25 @@
 <%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<c:url var="action" value="/member/join" />
+
 <h1>회원가입</h1>
-${message}
+<form method="POST" action="${action}">
+    <dl>
+        <dt>이메일</dt>
+        <dd>
+            <input type="text" name="email">
+        </dd>
+    </dl>
+    <dl>
+        <dt>비밀번호</dt>
+        <dd>
+            <input type="password" name="password">
+        </dd>
+    </dl>
+    <dl>
+        <dt>비밀번호 확인</dt>
+        <dd>
+            <input type="password" name="confirmPassword">
+        </dd>
+    </dl>
+</form>
