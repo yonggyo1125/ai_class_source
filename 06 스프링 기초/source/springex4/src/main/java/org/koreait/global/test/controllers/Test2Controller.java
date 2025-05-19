716deb0f5e602968556a93ecd1357832e8e97d24
yonggyo1125 <yonggyo00@kakao.com>
2025-05-19 17:13:23 +0900

    .

---
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/test/controllers/Test2Controller.java
@@ -0,0 +1,20 @@
+package org.koreait.global.test.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/test2")
+public class Test2Controller {
+
+    @GetMapping("/ex01")
+    public String exam01(Model model) {
+        model.addAttribute("programmings", List.of("자바", "파이썬", "자바스크립트", "러스트"));
+
+        return "test/test";
+    }
+}
