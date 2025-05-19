144eeae0d916bae53fc3ac3f098437bbf9dfa5e8
yonggyo1125 <yonggyo00@kakao.com>
2025-05-19 16:55:14 +0900

    .

---
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/test/controllers/Test1Controller.java
@@ -0,0 +1,44 @@
+package org.koreait.global.test.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test1")
+public class Test1Controller {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private HttpServletResponse response;
+
+    @Autowired
+    private HttpSession session;
+
+
+
+    @ResponseBody
+    @GetMapping("/ex01")
+    public void exam01(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        String URI = request.getRequestURI();
+        System.out.println(URI);
+
+        System.out.printf("request: %s, response: %s, session: %s%n", request, response, session);
+    }
+
+    @ResponseBody
+    @GetMapping("/ex02")
+    public void exam02() {
+        String URI = request.getRequestURI();
+        System.out.println(URI);
+
+        System.out.printf("request: %s, response: %s, session: %s%n", request, response, session);
+    }
+}
