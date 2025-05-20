358d081dfe5676ad75d51e2ae002717764c9c2b7
yonggyo1125 <yonggyo00@kakao.com>
2025-05-20 16:09:15 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/member/validators/JoinValidator.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/member/validators/JoinValidator.java
@@ -16,5 +16,23 @@ public class JoinValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
 
+        // Bean Validation API를 이용한 커맨드 객체 검증에 실패한 경우
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        /**
+         * 1. 중복 회원 체크
+         * 2. 비밀번호 확인
+         */
+
+        RequestJoin form = (RequestJoin) target;
+
+        // 2. 비밀번호 확인
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
+        if (!password.equals(confirmPassword)) {
+
+        }
     }
 }
