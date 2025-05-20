9248d821b532f5d76876785decfd5612eb5463f0
yonggyo1125 <yonggyo00@kakao.com>
2025-05-20 11:59:08 +0900

    .

---
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/configs/MessageSourceConfig.java
@@ -0,0 +1,20 @@
+package org.koreait.global.configs;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.addBasenames("messages.commons");
+        ms.setDefaultEncoding("UTF-8");
+
+        ms.setUseCodeAsDefaultMessage(true); // 코드가 등록되어 있지 않으면 코드를 기본 문자열로 반환
+        return ms;
+    }
+}
---
+++ 06 스프링 기초/source/springex4/src/test/java/exam03/Ex01.java
@@ -0,0 +1,26 @@
+package exam03;
+
+import org.junit.jupiter.api.Test;
+import org.koreait.global.configs.MvcConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringJUnitWebConfig(MvcConfig.class)
+public class Ex01 {
+    @Autowired
+    private MessageSource ms;
+
+    @Test
+    void test1() {
+        String email = ms.getMessage("email", null, null);
+        System.out.println(email);
+    }
+
+    @Test
+    void test2() {
+        Object[] args = {"사용자01", "user01@test.org"};
+        String loginMsg = ms.getMessage("LOGIN_MSG", args, null);
+        System.out.println(loginMsg);
+    }
+}
