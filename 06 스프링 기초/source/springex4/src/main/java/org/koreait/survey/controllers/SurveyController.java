4a9f1d04e1983a17f93627da81e32da580d43be0
yonggyo1125 <yonggyo00@kakao.com>
2025-05-21 17:24:06 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/survey/controllers/SurveyController.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/survey/controllers/SurveyController.java
@@ -1,11 +1,14 @@
 package org.koreait.survey.controllers;
 
+import org.koreait.member.entities.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/survey")
-@SessionAttributes("requestSurvey")
+@SessionAttributes({"requestSurvey", "member"})
 public class SurveyController {
 
     @ModelAttribute("requestSurvey")
@@ -13,8 +16,17 @@ public class SurveyController {
         return new RequestSurvey();
     }
 
+    @ModelAttribute("member")
+    public Member getMember() {
+        return new Member();
+    }
+
     @GetMapping("/page1")
-    public String page1() {
+    public String page1(Model model) {
+        Member member = getMember();
+        member.setName("page1에서 설정");
+        // 속성 값 member는 session 속성으로 추가
+        model.addAttribute("member", member);
 
         return "survey/page1";
     }
@@ -35,8 +47,11 @@ public class SurveyController {
     }
 
     @PostMapping("/done")
-    public String surveyPs(RequestSurvey form) {
+    public String surveyPs(@SessionAttribute(name="member", required=false) Member member, RequestSurvey form, SessionStatus status) {
         System.out.println("done:" + form);
+        System.out.println("member:" + member);
+        // 커맨드 객체 처리 완료 후 완료 표기, 더이상 세션 값으로 추가 X
+        status.setComplete();
 
         return "survey/done";
     }
