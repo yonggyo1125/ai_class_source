7a3a607382d27a1921b0d8447cae4eb4aa1c356f
yonggyo1125 <yonggyo00@kakao.com>
2025-05-21 14:37:48 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/member/validators/LoginValidator.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/member/validators/LoginValidator.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.member.controllers.RequestLogin;
 import org.koreait.member.entities.Member;
 import org.koreait.member.repositories.MemberRepository;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -40,5 +41,10 @@ public class LoginValidator implements Validator {
             errors.rejectValue("email", "NotFound");
         }
 
+        // 2. 조회된 회원의 비밀번호가 일치하는지 체크
+        if (member != null && !BCrypt.checkpw(password, member.getPassword())) {
+            errors.rejectValue("password", "Mismatch");
+
+        }
     }
 }
