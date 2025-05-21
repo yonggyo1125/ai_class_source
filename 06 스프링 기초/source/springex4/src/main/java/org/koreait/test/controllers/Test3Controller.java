87aacbacc16026bd94962b1b3a33a490da6ea689
yonggyo1125 <yonggyo00@kakao.com>
2025-05-21 10:57:08 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/test/controllers/Test3Controller.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/test/controllers/Test3Controller.java
@@ -17,9 +17,25 @@ public class Test3Controller {
     private final HttpServletResponse response;
 
     @ResponseBody
-    @GetMapping("/ex01")
+    @GetMapping("/sub/ex01")
     public void exam1() {
         Cookie cookie1 = new Cookie("key1", "value1");
         response.addCookie(cookie1);
+    }
+
+    @ResponseBody
+    @GetMapping("/sub2/ex01")
+    public void exam2() {
+        Cookie cookie1 = new Cookie("key2", "value2");
+        response.addCookie(cookie1);
+    }
+
+    @ResponseBody
+    @GetMapping("/sub3")
+    public void exam3() {
+        // /spring/test3
+        Cookie cookie = new Cookie("key3", "value3");
+        cookie.setPath(request.getContextPath() + "/test3");
+        response.addCookie(cookie);
     }
 }
