4843782d3d99eed35be0c577e08389ebea40ebc8
yonggyo1125 <yonggyo00@kakao.com>
2025-05-21 18:11:14 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/configs/InterceptorConfig.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/configs/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package org.koreait.global.configs;
 
 import lombok.RequiredArgsConstructor;
+import org.koreait.global.interceptors.CommonInterceptor;
 import org.koreait.global.interceptors.MemberOnlyInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,10 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class InterceptorConfig implements WebMvcConfigurer {
 
     private final MemberOnlyInterceptor memberOnlyInterceptor;
+    private final CommonInterceptor commonInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
        registry.addInterceptor(memberOnlyInterceptor)
                .addPathPatterns("/survey/**");
+
+//       registry.addInterceptor(commonInterceptor)
+//               .addPathPatterns("/**");
+
+        registry.addInterceptor(commonInterceptor); // addPathPatterns - /**
     }
 }
