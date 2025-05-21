9ae4b9f5ef1d571dc8a5a0587f372eea795afb5a
yonggyo1125 <yonggyo00@kakao.com>
2025-05-21 18:21:37 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/interceptors/CommonInterceptor.java
@@ -2,6 +2,7 @@ package org.koreait.global.interceptors;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +12,12 @@ public class CommonInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.printf("postHandle(), ModelAndView:%s%n", modelAndView);
+        //System.out.printf("postHandle(), ModelAndView:%s%n", modelAndView);
+        if (modelAndView != null) {
+            HttpSession session = request.getSession();
+            boolean isLogin = session.getAttribute("loggedMember") != null;
 
+            modelAndView.addObject("isLogin", isLogin);
+        }
     }
 }
--- 06 스프링 기초/source/springex4/src/main/webapp/WEB-INF/templates/main/index.jsp
+++ 06 스프링 기초/source/springex4/src/main/webapp/WEB-INF/templates/main/index.jsp
@@ -2,11 +2,11 @@
 <%@ taglib prefix="c" uri="jakarta.tags.core" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 
-<c:if test="${loggedMember == null}">
+<c:if test="${!isLogin}">
     <a href="<c:url value='/member/join' />">회원가입</a>
     <a href="<c:url value='/member/login' />">로그인</a>
 </c:if>
-<c:if test="${loggedMember != null}">
+<c:if test="${isLogin}">
     <spring:message code="LOGIN_MSG" arguments="${loggedMember.name},${loggedMember.email}" />
     <a href="<c:url value='/member/logout' />">로그아웃</a>
 </c:if>
