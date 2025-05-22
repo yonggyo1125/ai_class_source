7cdc973a912bad193a222161d85b1bc3858ff0a9
yonggyo1125 <yonggyo00@kakao.com>
2025-05-22 15:49:06 +0900

    .

--- 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/configs/FileConfig.java
+++ 06 스프링 기초/source/springex4/src/main/java/org/koreait/global/configs/FileConfig.java
@@ -1,15 +1,23 @@
 package org.koreait.global.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
+
+    @Value("${file.upload.url}")
+    private String url;
+
+    @Value("${file.upload.path}")
+    private String path;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///C:/uploads/");
+        registry.addResourceHandler(url + "**")
+                .addResourceLocations("file:///" + path);
                 // file:/// -> file://
     }
 }
