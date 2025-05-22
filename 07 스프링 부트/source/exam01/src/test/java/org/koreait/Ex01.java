8a231ea0f2e4b58515eb9957acf9c836a16c994e
yonggyo1125 <yonggyo00@kakao.com>
2025-05-22 17:35:07 +0900

    .

--- 07 스프링 부트/source/exam01/src/test/java/org/koreait/Ex01.java
+++ 07 스프링 부트/source/exam01/src/test/java/org/koreait/Ex01.java
@@ -1,7 +1,32 @@
 package org.koreait;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @SpringBootTest
 public class Ex01 {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void test1() throws Exception {
+        Connection con = dataSource.getConnection();
+        System.out.println(con);
+    }
+
+    @Test
+    void test2() {
+        String sql = "SELECT COUNT(*) FROM MEMBER";
+        long total = jdbcTemplate.queryForObject(sql, long.class);
+        System.out.println(total);
+    }
 }
