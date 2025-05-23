c96f6a7df2717386cc4c1706e7ae68ed8ec1e9fc
yonggyo1125 <yonggyo00@kakao.com>
2025-05-23 12:21:14 +0900

    .

--- 07 스프링 부트/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 07 스프링 부트/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,4 +1,17 @@
 package org.koreait.member.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
+
+    @ResponseBody
+    @GetMapping("/test")
+    public String test() {
+        return "테스트!";
+    }
 }
