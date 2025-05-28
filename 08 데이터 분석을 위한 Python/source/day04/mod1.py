2543b76092ac6704937935497bcf8564c8f73416
yonggyo1125 <yonggyo00@kakao.com>
2025-05-28 13:14:07 +0900

    .

--- 08 데이터 분석을 위한 Python/source/day04/mod1.py
+++ 08 데이터 분석을 위한 Python/source/day04/mod1.py
@@ -4,5 +4,8 @@ def add(a, b):
 def sub(a, b):
     return a - b
 
-print(add(10, 20))
-print(sub(20, 10))
+
+#print("__name__:", __name__) # 모듈명
+if __name__ == '__main__': # python mod1.py
+    print(add(10, 20))
+    print(sub(20, 10))
