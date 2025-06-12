a84effd158a8292a91ac8fe8cd7f78dd5e9c4405
yonggyo1125 <yonggyo00@kakao.com>
2025-06-12 17:20:48 +0900

    .

---
+++ 15 리액트/exam03/src/App.js
@@ -0,0 +1,11 @@
+import MyComponent from './components/MyComponent';
+
+const App = () => {
+  return (
+    <>
+      <MyComponent />
+    </>
+  );
+};
+
+export default App;
---
+++ 15 리액트/exam03/src/components/MyComponent.js
@@ -0,0 +1,5 @@
+const MyComponent = () => {
+  return <h1>나의 멋진 컴포넌트(함수)</h1>;
+};
+
+export default MyComponent;
