9dce965ecc556f8e6039e51d1f14235ac51300d6
yonggyo1125 <yonggyo00@kakao.com>
2025-06-13 18:08:20 +0900

    .

--- 15 리액트/todo/src/todo/components/TodoForm.js
+++ 15 리액트/todo/src/todo/components/TodoForm.js
@@ -1,5 +1,14 @@
-const TodoForm = () => {
-  return <h1>등록양식...</h1>;
+import { FaPlusSquare } from 'react-icons/fa';
+
+const TodoForm = ({ onSubmit, onChange }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" onChange={onChange}/>
+      <button type="submit">
+        <FaPlusSquare />
+      </button>
+    </form>
+  );
 };
 
 export default TodoForm;
--- 15 리액트/todo/src/todo/containers/TodoContainer.js
+++ 15 리액트/todo/src/todo/containers/TodoContainer.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import TodoForm from '../components/TodoForm';
 import TodoItems from '../components/TodoItems';
 
 const TodoContainer = () => {
+  const [todo, setTodo] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log('todo', todo);
+  };
+
+  const onChange = (e) => {
+    setTodo(e.target.value);
+  };
+
   return (
     <>
-      <TodoForm />
+      <TodoForm onSubmit={onSubmit} onChange={onChange} />
       <TodoItems />
     </>
   );
