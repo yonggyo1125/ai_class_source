8abc3ea1191e6df1f73529dd930f69d661b47f7c
yonggyo1125 <yonggyo00@kakao.com>
2025-06-16 18:21:41 +0900

    .

--- 15 리액트/todo/src/todo/components/TodoItems.js
+++ 15 리액트/todo/src/todo/components/TodoItems.js
@@ -3,8 +3,9 @@ import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
 const TodoItems = ({ items }) => {
   return (
     <ul>
-      {items.map(({ id, title, content }) => (
+      {items.map(({ id, title, content, checked }) => (
         <li key={id}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
           {title} / {content}
         </li>
       ))}
