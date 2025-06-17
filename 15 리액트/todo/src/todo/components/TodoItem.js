a0063f56c7dd2e42b6dfd82a076411a5dab719fd
yonggyo1125 <yonggyo00@kakao.com>
2025-06-17 16:57:08 +0900

    .

---
+++ 15 리액트/todo/src/todo/components/TodoItem.js
@@ -0,0 +1,7 @@
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+const TodoItem = (props) => {
+  console.log(props);
+  return <li></li>;
+};
+
+export default TodoItem;
--- 15 리액트/todo/src/todo/components/TodoItems.js
+++ 15 리액트/todo/src/todo/components/TodoItems.js
@@ -1,16 +1,32 @@
-import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import TodoItem from './TodoItem';
 
 const TodoItems = ({ items }) => {
   return (
     <ul>
-      {items.map(({ id, title, content, checked }) => (
-        <li key={id}>
-          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-          {title} / {content}
-        </li>
+      {items.map((props) => (
+        <TodoItem key={props.id} {...props} />
       ))}
     </ul>
   );
 };
+
+// const TodoItems = ({ items }) => {
+//   return (
+//     <ul>
+//       {items.map((props) => (
+//         <>
+//           <TodoItem {...props} />
+//           {/*<TodoItem id={id} title={title} content={content} checked={checked} /> */}
+//           {/*<li
+//           key={id} // 한줄 주석
+//         >
+//           {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+//           {title} / {content}
+//         </li> */}
+//         </>
+//       ))}
+//     </ul>
+//   );
+// };
 
 export default TodoItems;
