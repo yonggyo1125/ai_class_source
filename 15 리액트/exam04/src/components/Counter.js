1f2798933ebcac779e524bac937b3537d4717b10
yonggyo1125 <yonggyo00@kakao.com>
2025-06-18 16:49:54 +0900

    .

---
+++ 15 리액트/exam04/src/components/Counter.js
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+const Counter = () => {
+  const [number, setNumber] = useState(0);
+
+  const onIncrease = () => setNumber(number + 1);
+  const onDecrease = () => setNumber(number - 1);
+
+  return (
+    <>
+      <h1>{number}</h1>
+      <button type="button" onClick={onDecrease}>
+        -1
+      </button>
+      <button type="button" onClick={onIncrease}>
+        +1
+      </button>
+    </>
+  );
+};
+
+export default Counter;
