5257e2192e301851da3d5088aca4c7dd2b41a853
yonggyo1125 <yonggyo00@kakao.com>
2025-06-18 10:46:48 +0900

    .

--- 09 핀테크 머신러닝과 딥러닝 이해/source/diabetes/predict.py
+++ 09 핀테크 머신러닝과 딥러닝 이해/source/diabetes/predict.py
@@ -1,10 +1,17 @@
 import os
+import sys
 import json
 import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import SGDClassifier
 
+# 입력 데이터 처리 
+#data = '[[1.0, 46.0, 0.0, 0.0, 3.0, 29.6, 5.8, 130.0], [1.0, 69.0, 1.0, 0.0, 4.0, 29.52, 3.5, 140.0], [0.0, 50.0, 0.0, 0.0, 0.0, 27.32, 5.7, 140.0], [0.0, 74.0, 0.0, 0.0, 3.0, 25.53, 4.5, 159.0], [0.0, 80.0, 1.0, 0.0, 4.0, 21.17, 6.6, 145.0]]'
+if len(sys.argv) < 2:
+    sys.exit(-1)
+
+data = np.array(json.loads(sys.argv[1]))
 
 # 기준 경로 
 base_path = os.path.dirname(os.path.realpath(__file__)) + "/"
@@ -18,3 +25,6 @@ with open(base_path + "scaler.pkl", "rb") as f:
 
 
 # 모델 예측 
+data_scaled = scaler.transform(data)
+predictions = model.predict(data_scaled)
+print(json.dumps(predictions.tolist()))
