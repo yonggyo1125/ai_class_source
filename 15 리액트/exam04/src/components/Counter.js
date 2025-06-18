0dca2e0a7aa717fce321c877c75b9521f037769f
yonggyo1125 <yonggyo00@kakao.com>
2025-06-18 17:21:47 +0900

    .

--- 15 리액트/exam04/src/components/Counter.js
+++ 15 리액트/exam04/src/components/Counter.js
@@ -1,14 +1,26 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const Counter = () => {
   const [number, setNumber] = useState(0);
 
-  const onIncrease = () => setNumber(number + 1);
+  const cntRef = useRef(0);
+  const h1Ref = useRef();
+
+  useEffect(() => {
+    console.log('h1Ref.current', h1Ref.current);
+    h1Ref.current.style.background = 'orange';
+  }, [h1Ref]);
+
+  const onIncrease = () => {
+    setNumber(number + 1);
+    console.log('cntRef.current', cntRef.current);
+    cntRef.current++;
+  };
   const onDecrease = () => setNumber(number - 1);
 
   return (
     <>
-      <h1>{number}</h1>
+      <h1 ref={h1Ref}>{number}</h1>
       <button type="button" onClick={onDecrease}>
         -1
       </button>
