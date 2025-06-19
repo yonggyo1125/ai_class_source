9fe4b9320266700e8e1ee111a561c7cfa2650c29
yonggyo1125 <yonggyo00@kakao.com>
2025-06-19 17:21:59 +0900

    .

--- 15 리액트/exam04/src/App.js
+++ 15 리액트/exam04/src/App.js
@@ -3,10 +3,16 @@
 // const App = () => {
 //   return <Counter />;
 // };
-import Todo from './components/Todo';
+// import Todo from './components/Todo';
+
+// const App = () => {
+//   return <Todo />;
+// };
+
+import Counter2 from './components/Counter2';
 
 const App = () => {
-  return <Todo />;
+  return <Counter2 />;
 };
 
 export default App;
--- 15 리액트/exam04/src/components/Counter2.js
+++ 15 리액트/exam04/src/components/Counter2.js
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const Counter2 = () => {
     const [number, setNumber] = useState(0);
 
-    const onIncrease = () => setNumber(number+1);
-    const onDecrease = () => setNumber(number-1);
+    // const onIncrease = () => setNumber(number+1);
+    // const onDecrease = () => setNumber(number-1);
 
+    // const onIncrease = useCallback(() => setNumber(number+1), [number]);
+    // const onDecrease = useCallback(() => setNumber(number-1), [number]);
+    const onIncrease = useCallback(() => {
+        setNumber((number) => number + 1);
+    }, []);
+
+    const onDecrease = useCallback(() => {
+        setNumber((number) => number - 1);
+    }, [])
     return (
         <>
             <h1>{number}</h1>
