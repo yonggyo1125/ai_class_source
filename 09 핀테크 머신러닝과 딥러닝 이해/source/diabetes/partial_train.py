7ce9f5f9357e7792fb16460e91b18af39ca519bf
yonggyo1125 <yonggyo00@kakao.com>
2025-06-19 13:21:41 +0900

    .

--- 09 핀테크 머신러닝과 딥러닝 이해/source/diabetes/partial_train.py
+++ 09 핀테크 머신러닝과 딥러닝 이해/source/diabetes/partial_train.py
@@ -28,17 +28,47 @@ with open(base_path + "scaler.pkl", "rb") as f:
 
 # DB 연결 및 추가학습할 데이터 조회
 items = []
+targets = []
 gender = {'FEMALE': 0, 'MALE': 1, 'OTHER': 2}
 SH = {'NO_INFO': 0, 'CURRENT': 1, 'EVER': 2, 'FORMER': 3, 'NEVER': 4, 'NOT_CURRENT': 5}
 with pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=database, charset='utf8') as db:
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM SURVEY_DIABETES WHERE trainDone IS NULL OR trainDone = 0")
-    for row in cursor.fetchall():
-        item = list(row[2:-4])
+    cursor.execute("SELECT gender, age, hypertension, heartDisease, smokingHistory, bmi, hbA1c, bloodGlucoseLevel, diabetes FROM SURVEY_DIABETES WHERE trainDone IS NULL OR trainDone = 0")
+    rows = cursor.fetchall()
+    if len(rows) == 0:
+        print("학습할 데이터가 없습니다.")
+        sys.exit()
+
+    for row in rows:
+        item = list(row)
         item[0] = gender[item[0]]
         item[4] = SH[item[4]]
 
-        items.append(item)
+        items.append(item[:-1])
+        targets.append(item[-1])
 
 
-print(items)
+# 훈련데이터, 타겟 데이터를 분리
+train_input = np.array(items)
+train_target = np.array(targets)
+
+# 훈련데이터를 표준 점수로 정규화
+train_scaled = scaler.transform(train_input)
+
+# 부분학습 진행 
+model.partial_fit(train_scaled, train_target);
+
+# 정확도(R^2) 검증
+print("정확도:", model.score(train_scaled, train_target)) 
+
+# 모델 저장 
+with open(base_path + "model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+# 학습 완료 DB 업데이트
+with pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=database, charset='utf8') as db:
+    cursor = db.cursor()
+    cursor.execute("UPDATE SURVEY_DIABETES SET trainDone=1 WHERE trainDone IS NULL OR trainDone=0")
+    db.commit()
+
+print("부분학습 완료!")
