c676bfa9c30a72bbad84c8b53cf7b13056338c28
yonggyo1125 <yonggyo00@kakao.com>
2025-06-20 16:43:35 +0900

    .

--- 15 리액트/exam04/src/components/Counter3.js
+++ 15 리액트/exam04/src/components/Counter3.js
@@ -11,11 +11,23 @@ const Counter3 = () => {
 
   const { number } = state;
 
+  const onIncrease = useCallback(() => {
+    dispatch({ type: 'INCREASE' });
+  }, []);
+
+  const onDecrease = useCallback(() => {
+    dispatch({ type: 'DECREASE' });
+  }, []);
+
   return (
     <>
       <h1>{number}</h1>
-      <button type="button">-1</button>
-      <button type="button">+1</button>
+      <button type="button" onClick={onDecrease}>
+        -1
+      </button>
+      <button type="button" onClick={onIncrease}>
+        +1
+      </button>
     </>
   );
 };
