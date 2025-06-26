d96c30689441aa1e93f94357617d555d925c51b1
yonggyo1125 <yonggyo00@kakao.com>
2025-06-26 18:06:36 +0900

    .

--- 15 리액트/exam05/src/CSSModule/CSSModule.js
+++ 15 리액트/exam05/src/CSSModule/CSSModule.js
@@ -1,5 +1,13 @@
+import styles from './CSSModule.module.scss';
+console.log(styles);
+
 const CSSModule = () => {
-  return <div>안녕하세요, 저는 CSSModule!, 반갑습니다.</div>;
+  return (
+    <div className={`${styles.wrapper}`}>
+      안녕하세요, 저는 <span className="something">CSSModule!</span>,
+      반갑습니다.
+    </div>
+  );
 };
 
 export default CSSModule;
---
+++ 15 리액트/exam05/src/classNames/MyComponent1.js
@@ -0,0 +1,9 @@
+import './MyComponent1.css';
+import { useState } from 'react';
+import classNames from 'classnames';
+
+const MyComponent1 = () => {
+  const [visible, setVisible] = useState(false);
+};
+
+export default MyComponent1;
