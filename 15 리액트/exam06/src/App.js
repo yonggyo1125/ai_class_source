c90e1882f1b026cbe09b3107661aa1299110e30c
yonggyo1125 <yonggyo00@kakao.com>
2025-07-02 11:01:09 +0900

    .

--- 15 리액트/exam06/src/App.js
+++ 15 리액트/exam06/src/App.js
@@ -3,17 +3,21 @@ import MainPage from './main/pages/MainPage';
 import MemberRoutes from './global/routes/MemberRoutes';
 import BoardRoutes from './global/routes/BoardRoutes';
 import MainLayout from './global/layouts/MainLayout';
+import NotFoundPage from './global/pages/NotFoundPage';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<MainLayout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<MainPage />} />
         </Route>
       </Routes>
       <MemberRoutes />
       <BoardRoutes />
+      <Routes>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 };
---
+++ 15 리액트/exam06/src/global/pages/NotFoundPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return <h1>페이지 없는데..</h1>;
+};
+
+export default React.memo(NotFoundPage);
