773216ffbd75b6259f1addc5eb5f747e44fe5025
yonggyo1125 <yonggyo00@kakao.com>
2025-07-10 16:04:12 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 11. 금융 OpenAPI 활용/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,23 +1,31 @@
 package org.koreait.member.controllers;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/member")
 public class MemberController {
 
     @PostMapping("/join")
-    public void join(@RequestBody @Valid RequestJoin form, Errors errors) {
+    public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
+
+        ResponseEntity<Object> res = new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
         if (errors.hasErrors()) {
-            errors.getFieldErrors().stream().forEach(e -> System.out.println(e.getDefaultMessage()));
+            List<String> errorMessages = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
+
+            res = new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
         }
 
-        System.out.println(form);
+        return res;
     }
 }
