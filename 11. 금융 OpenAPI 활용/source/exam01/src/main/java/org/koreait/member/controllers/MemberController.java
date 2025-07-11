2584c07ed4b02339ddccf19b79668ae54cf0a3ca
yonggyo1125 <yonggyo00@kakao.com>
2025-07-11 09:41:20 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
+++ 11. 금융 OpenAPI 활용/source/exam01/src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,7 +1,6 @@
 package org.koreait.member.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -18,19 +17,27 @@ public class MemberController {
 
     @PostMapping("/join")
     public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("header1", "value1");
-        headers.add("header2", "value2");
-
-        ResponseEntity<Object> res = new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
-
         if (errors.hasErrors()) {
-            List<String> errorMessages = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
-
-            res = new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+            List<String> errorMessags = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessags);
         }
 
-        return res;
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+//    public ResponseEntity<Object> join(@RequestBody @Valid RequestJoin form, Errors errors) {
+//
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.add("header1", "value1");
+//        headers.add("header2", "value2");
+//
+//        ResponseEntity<Object> res = new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
+//
+//        if (errors.hasErrors()) {
+//            List<String> errorMessages = errors.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
+//
+//            res = new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return res;
+//    }
 }
