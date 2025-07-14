c24e7867432819dd3303eaf9d2824d8f7ee831b5
yonggyo1125 <yonggyo00@kakao.com>
2025-07-14 11:12:59 +0900

    .

---
+++ 11. 금융 OpenAPI 활용/source/exam02/src/main/java/org/koreait/member/entities/Member.java
@@ -0,0 +1,20 @@
+package org.koreait.member.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Member {
+    @Id
+    private Long seq;
+
+    private String email;
+    private String password;
+    private String name;
+
+    private LocalDateTime createdAt;
+}
---
+++ 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex01.java
@@ -0,0 +1,28 @@
+package org.koreait.tests;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.koreait.member.entities.Member;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class Ex01 {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    void test1() {
+        Member member = new Member();
+        member.setSeq(1L);
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
+        member.setName("사용자01");
+        member.setCreatedAt(LocalDateTime.now());
+
+        em.persist(member); // 영속 상태로 만들기, 엔티티의 변화감지를 할수 있도록 만든것
+    }
+}
