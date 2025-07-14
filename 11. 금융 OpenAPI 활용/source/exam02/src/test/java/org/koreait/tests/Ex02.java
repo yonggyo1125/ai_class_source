bc256343dc7e01503af8492f010037bd569bd235
yonggyo1125 <yonggyo00@kakao.com>
2025-07-14 11:58:52 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex02.java
+++ 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex02.java
@@ -1,4 +1,39 @@
 package org.koreait.tests;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.koreait.member.entities.Member;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles({"default", "test"})
 public class Ex02 {
+    @PersistenceContext
+    private EntityManager em;
+
+    @BeforeEach
+    void init() {
+        Member member = new Member();
+        member.setSeq(1L);
+        member.setEmail("user01@test.org");
+        member.setPassword("12345678");
+        member.setName("사용자01");
+        member.setCreatedAt(LocalDateTime.now());
+
+        em.persist(member);
+        em.flush();
+    }
+
+    @Test
+    void test1() {
+        Member member = em.find(Member.class, 1L);
+        System.out.println(member);
+    }
 }
