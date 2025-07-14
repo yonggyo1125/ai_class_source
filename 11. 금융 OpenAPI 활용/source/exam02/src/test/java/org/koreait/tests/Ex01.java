5b70e68799570fdd25d1e1b71506f11f5adbd9da
yonggyo1125 <yonggyo00@kakao.com>
2025-07-14 11:16:12 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex01.java
+++ 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex01.java
@@ -2,6 +2,8 @@ package org.koreait.tests;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.koreait.member.entities.Member;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,8 +16,18 @@ public class Ex01 {
     @PersistenceContext
     private EntityManager em;
 
+    @BeforeEach
+    void init() {
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    void after() {
+        em.getTransaction().commit();
+    }
     @Test
     void test1() {
+
         Member member = new Member();
         member.setSeq(1L);
         member.setEmail("user01@test.org");
@@ -24,5 +36,6 @@ public class Ex01 {
         member.setCreatedAt(LocalDateTime.now());
 
         em.persist(member); // 영속 상태로 만들기, 엔티티의 변화감지를 할수 있도록 만든것
+        em.flush(); // 변화 상태를 DB에 영구 반영
     }
 }
