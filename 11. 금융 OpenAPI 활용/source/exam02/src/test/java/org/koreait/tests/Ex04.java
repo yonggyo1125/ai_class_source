05397376b704b34a44c2051869326f701422ea64
yonggyo1125 <yonggyo00@kakao.com>
2025-07-15 15:22:31 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex04.java
+++ 11. 금융 OpenAPI 활용/source/exam02/src/test/java/org/koreait/tests/Ex04.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 @ActiveProfiles({"default", "test"})
 public class Ex04 {
@@ -18,20 +21,24 @@ public class Ex04 {
 
     @BeforeEach
     void init() {
+        List<Member> members = new ArrayList<>();
         for (long i = 1; i <= 10; i++) {
             Member member = new Member();
             member.setName("사용자" + i);
             member.setPassword("11111111");
             member.setAuthority(Authority.MEMBER);
             member.setEmail("user" + i + "@test.org");
-            repository.save(member);// em.persist(...)
+            members.add(member);
+            //repository.save(member);// em.persist(...)
         }
-
-        repository.flush();
+        repository.saveAllAndFlush(members);
+        //repository.saveAll(members);
+        //repository.flush();
     }
 
     @Test
     void test1() {
-
+        List<Member> members = repository.findAll();
+        members.forEach(System.out::println);
     }
 }
