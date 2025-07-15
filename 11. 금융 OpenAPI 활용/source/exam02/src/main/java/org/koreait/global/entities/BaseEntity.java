041142b30f28b1519fca2c89263757f71ec8b698
yonggyo1125 <yonggyo00@kakao.com>
2025-07-15 12:19:10 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam02/src/main/java/org/koreait/global/entities/BaseEntity.java
+++ 11. 금융 OpenAPI 활용/source/exam02/src/main/java/org/koreait/global/entities/BaseEntity.java
@@ -1,19 +1,25 @@
 package org.koreait.global.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Data
 @MappedSuperclass // 공통 속성으로 사용될 상위 클래스임을 알려주는 애노테이션
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @CreationTimestamp
+    @CreatedDate
+    @Column(updatable = false) // 생성일자는 최초 1번만 등록
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
+    @LastModifiedDate
+    @Column(insertable = false) // 수정일자는 최초에는 등록  X
     private LocalDateTime modifiedAt;
 }
