2c73ac4b672e217d8e1351bacb4c94ffad7b27bc
yonggyo1125 <yonggyo00@kakao.com>
2025-07-17 11:21:14 +0900

    .

--- 11. 금융 OpenAPI 활용/source/exam02/src/main/java/org/koreait/board/repositories/BoardDataRepository.java
+++ 11. 금융 OpenAPI 활용/source/exam02/src/main/java/org/koreait/board/repositories/BoardDataRepository.java
@@ -1,6 +1,7 @@
 package org.koreait.board.repositories;
 
 import org.koreait.board.entities.BoardData;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
@@ -9,6 +10,7 @@ import java.util.List;
 
 public interface BoardDataRepository extends JpaRepository<BoardData, Long>, QuerydslPredicateExecutor<BoardData> {
 
+    @EntityGraph(attributePaths = "member")
     List<BoardData> findBySubjectContaining(String keyword);
 
     @Query("SELECT b FROM BoardData b LEFT JOIN b.member")
