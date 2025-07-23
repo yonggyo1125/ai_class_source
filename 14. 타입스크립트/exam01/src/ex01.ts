238319d97731abfad5e614e6a11ad32b97c1df4a
yonggyo1125 <yonggyo00@kakao.com>
2025-07-23 10:23:10 +0900

    .

---
+++ 14. 타입스크립트/exam01/src/ex01.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string) {
+  console.log(`${name}님 안녕하세요.`)
+}
+
+sayHello('김이름')
+//sayHello(1234);
---
+++ 14. 타입스크립트/exam01/src/ex02.ts
@@ -0,0 +1,13 @@
+// 원시 타입 자료형 : number, string, boolean, null, undefined ..
+let employeeName: string = '이이름'
+
+// 배열
+const fruits: string[] = [] // 문자열로  된 값만 배열에 추가 가능
+fruits.push('사과')
+//fruits.push(1111)
+
+// 배열 - string, number 자료형으로 한정하는 경우 - Union 연산자 | 이용
+const fruits2: (string | number)[] = [1, '사과']
+
+// 배열 - 자료형의 갯수와 순서를 고정 - 튜플
+const fruits3: [string, number] = ['사과', 10]
