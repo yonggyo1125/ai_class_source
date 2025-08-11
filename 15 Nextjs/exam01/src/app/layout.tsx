2e1becff2c409ace46249c9efeb022c1518c36b0
yonggyo1125 <yonggyo00@kakao.com>
2025-08-11 12:13:19 +0900

    .

--- 15 Nextjs/exam01/src/app/layout.tsx
+++ 15 Nextjs/exam01/src/app/layout.tsx
@@ -5,7 +5,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header>상단 헤더 영역</header>
+        {children}
+        <footer>하단 푸터 영역</footer>
+      </body>
     </html>
   )
 }
